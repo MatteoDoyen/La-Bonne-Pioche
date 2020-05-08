17e4ac122ff44b8336b2ac27723d9492bb4311c4
BaptSeg <segeat.b@gmail.com>
2020-05-08 19:05:37 +0200

    Navbar fonctionnelles

---
+++ view/accueil.test.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>La bonne Pioche</title>
+    <link rel="stylesheet" href="../framework/bootstrap-4.4.1-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="css/accueil.test.css">
+  </head>
+  <body>
+
+    <script defer src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+      integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script defer src="../framework/bootstrap-4.4.1-dist/js/bootstrap.bundle.min.js"></script>
+
+    <div class="container-fluid">
+
+      <div class="fixed-top">
+        <div class="row m_nav-bar">
+          <div class="titre offset-lg-2 col-lg-8 offset-1 col-10">
+            <h1>La bonne pioche</h1>
+            <h2>L'épicerie grenobloise de produits locaux sans emballage</h2>
+          </div>
+        </div>
+
+        <nav class="row navbar navbar-expand-lg m_nav-bar">
+          <a class="navbar-brand" href="#">
+            <img src="../data/img/logo.png" alt="Logo">
+          </a>
+          <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">
+                <i class="fa fa-navicon" style="color:#fff; font-size:28px;"></i>
+            </span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Accueil</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link with-border" href="#">Nous</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link with-border" href="#">Mode d'emploi</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link with-border" href="#">Produits</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link with-border" href="#">Actualités</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link with-border" href="#">Nous trouver</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Paniers</a>
+              </li>
+            </ul>
+            <div class="form-inline my-2 my-lg-0">
+              <button class="btn btn-success my-2 my-sm-0 connect" type="button" name="button">Se connecter</button>
+            </div>
+          </div>
+        </nav>
+      </div>
+
+
+
+      <div class="row">
+        <img class="image_accueil" src="../data/img/image-acceuil.jpg" alt="Image d'accueil">
+      </div>
+
+    </div>
+
+  </body>
+</html>
