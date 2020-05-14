c5d0f4148c642e6e1c8477b250fd2ed6549deda9
JeanCulette <cfricaud@gmail.com>
2020-05-14 20:50:36 +0200

    bdd paniers + images & produits/panier

---
+++ data/create_paniers.sql
@@ -0,0 +1,7 @@
+CREATE TABLE paniers (
+  libelle STRING,
+  id_Panier INTEGER PRIMARY KEY,
+  coefficient INTEGER,
+  prix FLOAT,
+  image STRING
+ );
---
+++ data/create_produits_paniers.sql
@@ -0,0 +1,4 @@
+CREATE TABLE produits_paniers (
+  id INTEGER PRIMARY KEY,
+  id_Panier INTEGER PRIMARY KEY,
+ );
