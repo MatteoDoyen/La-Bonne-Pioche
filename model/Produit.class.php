f626076aad5b7b00f7055e5feb02e44b78cbecca
JeanCulette <cfricaud@gmail.com>
2020-05-14 13:27:53 +0200

    classe Produit & ProduitDAO

--- model/Produit.class.php
+++ model/Produit.class.php
@@ -11,26 +11,32 @@ class Produit {
   private $coef;
   private $description;
   private $origine;
-  private $caractéristiques;
+  private $caracteristiques;
   private $prix_u;
+  private $url_img;
   // Chemin URL à ajouter pour avoir l'image du produit
   private const URL = '../data/img/img_produits/';
 
-  function __construct(int $id=0,string $author='',string $title='',string $cover='',string $mp3='',string $category='') {
+  function __construct(int $stock=0,int $stock=0,string $libelle='',string $fabricant='',
+  string $rayon='',string $famille='',double $coef=1; string $description='',
+  string $origine='', string $caracteristiques='', float $prix_u, string $url_img='') {
+      $this -> stock = $stock;
       $this -> id =  $id;
-      $this -> author =  $author;
-      $this -> title =  $title;
-      $this -> cover =  $cover;
-      $this -> mp3 = $mp3;
-      $this -> category = $category;
+      $this -> libelle =  $libelle;
+      $this -> fabricant =  $fabricant;
+      $this -> rayon =  $rayon;
+      $this -> famille = $famille;
+      $this -> coef = $coef;
+      $this -> description = $description;
+      $this -> origine = $origine;
+      $this -> caractéristiques = $caracteristiques;
+      $this -> prix_u = $prix_u;
+      $this -> url_img = $url_img;
   }
 
-  public function __get($name){
-    if($name == "cover"){
-      return Music::URL.'img/'.$this->$name;
-    }
-    else if($name == "mp3"){
-      return Music::URL."mp3/".$this->$name;
+  public function __get($libelle){
+    if($name == "url_img"){
+      return Produit::URL.$this->$libelle;
     }
     else {
       return $this -> $name;
---
+++ model/ProduitDAO.class.php
@@ -0,0 +1,29 @@
+<?php
+require_once(dirname(__FILE__).'/Produit.class.php');
+
+// Le Data Access Objet
+class ProduitDAO {
+  private $db;
+
+  // Constructeur chargé d'ouvrir la BD
+  function __construct() {
+    $this->db = new PDO('sqlite:'.dirname(__FILE__).'/../data/produits.csv','',''); //SÉPARATEURS À VÉRIFIER
+    $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+
+  // Accès à une musique
+  function get(int $id) : Produit {
+    $req = "SELECT * FROM produits WHERE id = '$id'";
+    $sth = $this->db->query($req);
+    $resArray= $sth->fetchAll(PDO::FETCH_ASSOC);
+    foreach($resArray as $row)
+    {
+      $produit = new Produit($row['stock'],$row['id'],$row['libelle'],$row['fabricant'],$row['rayon'],$row['famille'],
+      $row['coef'],$row['description'],$row['origine'],$row['caracteristiques'],$row['prix_u'],$row['url_img']);
+    }
+    return $produit;
+  }
+
+}
+
+?>
