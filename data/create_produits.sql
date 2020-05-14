691f53d7901148a0b4edd5b817b06b39bc43d1dd
JeanCulette <cfricaud@gmail.com>
2020-05-14 13:48:02 +0200

    produit bd

---
+++ data/create_produits.sql
@@ -0,0 +1,13 @@
+CREATE TABLE produits (
+  stock INTEGER,
+  id INTEGER PRIMARY KEY,
+  libelle STRING,
+  fabricant STRING,
+  rayon STRING,
+  famille STRING,
+  description STRING,
+  origine STRING,
+  caracteristiques STRING,
+  prix_u FLOAT,
+  url_img STRING
+ );
