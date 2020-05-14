acfe27230255700013fba0024064e8352e44c5fe
JeanCulette <cfricaud@gmail.com>
2020-05-14 12:45:45 +0200

    initialisation front produits

--- view/navbar.php
+++ view/navbar.php
@@ -27,7 +27,7 @@
           <a class="nav-link with-border mode_emploie" href="#">Mode d'emploi</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link with-border produits" href="#">Produits</a>
+          <a class="nav-link with-border produits" href="../controlers/produits.ctrl.php">Produits</a>
         </li>
         <li class="nav-item">
           <a class="nav-link with-border actualites" href="#">Actualités</a>
