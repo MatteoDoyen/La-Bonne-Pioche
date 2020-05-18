fecb0302b0ec1476708ba284ca5aa6a0d4a05542
JeanCulette <cfricaud@gmail.com>
2020-05-18 21:57:44 +0200

    Create create_utilisateur.sql

---
+++ data/create_utilisateur.sql
@@ -0,0 +1,14 @@
+CREATE TABLE utilisateurs (
+  refUtilisateur INTEGER PRIMARY KEY,
+  nom STRING,
+  prenom STRING,
+  adresseMail STRING,
+  mdp STRING,
+  etat STRING,
+  numeroTelephone STRING,
+  statut BOOLEAN,
+  newsletter BOOLEAN,
+  genre BOOLEAN,
+  tauxReduction FLOAT,
+  refEntreprise INTEGER,
+ );
