ecc464ec9340a41370b0e273b0af431944a6ac91
JeanCulette <cfricaud@gmail.com>
2020-05-19 15:51:57 +0200

    Update PanierDAO.class.php

--- model/PanierDAO.class.php
+++ model/PanierDAO.class.php
@@ -56,7 +56,7 @@ class PanierDAO {
     $resArray= $sth->fetchAll(PDO::FETCH_ASSOC);
     foreach($resArray as $row)
     {
-      $panier = new Panier($row['libelle'],$row['id_Panier'],$row['coefficient'],$row['prix'],$row['image']);
+      $panier = new Panier($row['libelle'],$row['id_Panier'],$row['coefficient'],$row['prix'],$row['image'],$row['nb_bocaux'],););
     }
     return $panier;
   }
