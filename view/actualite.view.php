185a9eb65ca05b24dbaa6950899b283e4f225ae3
LeDernierDeNous <mouloud3826@gmail.com>
2020-05-20 11:17:06 +0200

    actualité first template

--- view/actualite.view.php
+++ view/actualite.view.php
@@ -16,37 +16,115 @@
       <?php include("../view/navbar.php") ?>
 
       <div class="row info">
-        <div class="offset-lg-2 col-lg-8 text-center">
+        <div class="col-lg-12 text-center">
           <div class="mb-3">
-            <h4 class="m-0">Horraires</h4>
-            <p class="mb-1">Du lundi au samedi de 10h à 20h</p>
-            <p class="mb-1">Sauf le jeudi de 10h à 14h et de 15h à 20h</p>
-            <p>Et sauf les jours fériés !</p>
+            <h4 class="m-0">BONJOUR A TOUS !</h4>
+            <p class="mb-1">Nous espérons que vous êtes tous en forme !</p>
+            <p class="mb-1">Voici les arrivages de légumes de la semaines du 18 mai 2020 disponible à la commande :</p>
           </div>
-          <div class="mb-3">
-            <h4 class="m-0">Adresse</h4>
-            <p class="mb-1">2, rue Condillac</p>
-            <p>38000 Grenoble</p>
-          </div>
-          <div class="mb-3">
-            <h4 class="m-0">Pour venir nous voir</h4>
-            <p class="mb-1">Tram A et B : Arrêt Hubert Dubedout - Maison du Tourisme</p>
-            <p class="mb-1">Tous les bus allant au centre ville</p>
-            <p class="mb-1">Parking vélo à promixité</p>
-            <p>Parking voiture (gratuit pendant 20 minutes)</p>
-          </div>
-          <div class="mb-3">
-            <h4 class="m-0">Téléphone</h4>
-            <p>04 38 38 31 22</p>
-          </div>
-          <div>
-            <h4 class="m-0">Email</h4>
-            <p>labonnepioche38@gmail.com</p>
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-2">
+              <div class="mb-3">
+                <h4 class="m-0">Lundi 18 à partir de 15h :</h4>
+                <p class="mb-1">pommes de La Terrasse, noix de Tèche, fraises bio de Montbonnot (barquette 250g), salades bio de Montbonnot, roquette bio de Meylan, pommes de terre nouvelle bio de Meylan, courgettes bio de Meylan, carotte botte bio de Montbonnot, choux pointu de Montbonnot, oignons bottes bio de Meylan,  pommes de terre de Tèche, ail bio, échalotes bio</p>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-2">
+            <div class="mb-3">
+                <h4 class="m-0">Mardi 19 :</h4>
+                <p class="mb-1">pommes de La Terrasse, noix de Tèche, fraises d’Ardèche (barquette 500g), asperges de Bernin, salades bio de Montbonnot, pommes de terre nouvelle bio de Meylan, endives et champignons de paris de St Hilaire de la Côte, blette bio de Montbonnot, carottes botte de Montbonnot, courgettes bio de Meylan, choux pointu de Montbonnot, oignons bottes bio de Meylan, pommes de terre de Tèche, ail bio, échalotes bio</p>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-2">
+            <div class="mb-3">
+                <h4 class="m-0">Mercredi 20 :</h4>
+                <p class="mb-1">pommes, noix de Tèche, fraises d’Ardèche (barquette 500g), asperges de Bernin, salades bio de Montbonnot, pommes de terre nouvelle bio, endives et champignons de paris de St Hilaire de la Côte, persil & coriandre & aneth en cours de conversion bio de Vaulnaveys, carottes botte bio de Montbonnot, courgettes bio, choux pointu de Montbonnot, oignons bottes bio de Meylan, aillet botte bio de Montbonnot, pommes de terre de Tèche, ail bio, échalotes bio</p>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-2">
+            <div class="mb-3">
+                <h4 class="m-0">Jeudi 21 :</h4>
+                <p class="mb-1">C'est férié ! On sera donc fermé ;)</p>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-2">
+            <div class="mb-3">
+                <h4 class="m-0">Vendredi 22 :</h4>
+                <p class="mb-1">pommes, noix de Tèche, fraises d’Ardèche (barquette 500g), salades bio de Montbonnot, roquette bio de Meylan, endives de St Hilaire de la Côte, pommes de terre nouvelle bio, blette bio de Montbonnot, carottes botte bio de Montbonnot, courgettes bio, choux pointu de Montbonnot,  oignons bottes bio de Meylan, pommes de terre de Tèche, ail bio, échalotes bio</p>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-2">
+            <div class="mb-3">
+                <h4 class="m-0">Samedi 23 :</h4>
+                <p class="mb-1">pommes, noix de Tèche, fraises d’Ardèche (barquette 500g), salades bio de Montbonnot, pommes de terre nouvelle bio, carottes botte bio de Montbonnot, courgettes bio, choux pointu de Montbonnot, oignons bottes bio de Meylan, pommes de terre de Tèche, ail bio, échalotes bio</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
 
       <hr class="offset-lg-3 col-lg-6 offset-md-3 col-md-6 offset-sm-2 col-sm-8 offset-1 col-10 my-5">
+
+      <div class="row info">
+        <div class="col-lg-12">
+          <div class="row">
+            <div class="col-lg-12 col-xl-6">
+              <div class="mb-3">
+                <h4 class="m-0">ATTENTION LES HORAIRES ONT UN PEU CHANGE EN CETTE SITUATION INEDITE !</h4>
+                <p class="mb-1">A partir du 11 mai, nous continuerons (au moins jusqu'au 25 mai) à ouvrir le magasin du lundi au vendredi de 10h à 18h non stop et le samedi de 9h à 13h !
+                Vous avez également la possibilité de passer commande par mail : labonnepioche38@gmail.com soit :
+                - pour la récupérer en magasin entre 10h et 18h du lundi au vendredi ou le samedi de 09h à 13h ;
+                - pour la récupérer chez notre super point relais : Le Bon Label au 39 rue Humbert II quartier Championnet à Grenoble de 18h à 20h du mardi au vendredi et le samedi de 16h à 19h !</p>
+              </div>
+            </div>
+            <div class="col-lg-12 col-xl-6">
+              <div class="mb-3">
+                <h4 class="m-0">LES COMMANDES COMMENT CA MARCHE ?</h4>
+                <p class="mb-1">1- vous nous envoyez un mail labonnepioche38@gmail.com avec le détail de tous les produits que vous souhaitez commander. Pour vous aider, allez consulter la page PRODUITS du site : https://produits.labonnepiochegrenoble.com/ Sur notre site, on commence à avoir une bonne base de produits en ligne mais pas encore tous car cela prend du temps et nous n'en avons pas forcément beaucoup en ce moment donc n'hésitez pas à le re-consulter de temps en temps car on ajoute les produits au fil de jours ! En revanche, désolé mais les prix ne sont pas encore affichés sur notre site. N'hésitez pas dans votre mail à nous précisez que vous souhaiteriez avoir les prix de tel ou tel produit et nous vous les donnerons avec plaisir ! </p>
+                <p class="mb-1">2- nous vous envoyons un mail pour vous confirmer la bonne réception de la commande en vous indiquant les produits éventuellement épuisés ainsi que le jour et l'heure de récupération au magasin. La récupération sera possible au minimum 24h après la confirmation de votre commande pour nous laisser le temps de la préparer. Si par exemple, vous passez commande le mardi soir à 10h pour une commande pour le lendemain matin, nous ne pourrons pas répondre favorablement. Par contre, si vous passez commande le mardi soir à 10h pour une commande pour le jeudi matin, cela ne pose pas de problème.</p>
+                <p class="mb-1">3- nous vous préparons votre commande soit dans des sachets krafts ou bouteille le cas échéant soit directement dans vos contenants qu'il suffira de nous déposer la veille au magasin. Merci de bien indiquer votre nom sur le sac, et le nom de chaque produit sur vos contenants pour nous faire gagner un temps précieux dans la préparation. </p>
+                <p class="mb-1">4- vous venez chercher votre commande le jour indiqué aux alentours de l'heure indiquée. Si il y a la queue en dehors du magasin, vous passez prioritaire à condition de ne rien avoir à ajouter à votre commande et paierez par CB directement à la caisse du magasin. Si vous avez des choses à rajouter, on vous laissera faire la queue désolé !</p>
+              </div>
+            </div>
+            <div class="col-lg-12 col-xl-6">
+              <div class="mb-3">
+                <h4 class="m-0">LE RETRAIT DES COMMANDES AU BON LABEL COMMENT CA MARCHE ?</h4>
+                <p class="mb-1">Cela fonctionne de la même manière qu'une commande normale. En revanche, merci de nous donner 24h de délai supplémentaire pour que nous puissions organiser la logistique ! Dès que votre commande est prête, nous vous enverrons un mail avec la facture jointe. En allant chercher votre commande, vous donnerez un chèque du montant correspondant à la facture reçue ! Le plus du retrait au Bon Label : vous avez la possibilité de commander le plat du soir de leur carte en le récupérant dans vos propres contenants ! Tous les plats sont fait maison, locaux et élaborés avec amour ;) Au menu, pour cette semaine : - mardi 19 mai : Pain de courgette au basilic et sa salade - mercredi 20 mai : Lasagnes - jeudi 21 mai : c'est férié ! Ils seront aussi fermés ! - vendredi 22 mai : Fish & Chips (Truite du Vercors) - samedi 23 mai : Burger au Bleu du Vercors & Coppa </p>
+              </div>
+            </div>
+            <div class="col-lg-12 col-xl-6">
+              <div class="mb-3">
+                <h4 class="m-0">​LES MESURES SANITAIRES EN MAGASIN !</h4>
+                <p class="mb-1">Nous essayons de faire face à cette situation inédite et faisons le maximum pour continuer à vous servir dans les meilleures conditions. Nous mettons en oeuvre toutes les mesures sanitaires réalisables dans le magasin : - nous limitons le nombre de personne en magasin à 5  - toutes les pelles et pinces ne servent qu'à 1 seule personne et 1 seul produit (pour éviter de mélanger les allergènes) et sont nettoyées ensuite - nous désinfectons 2 fois par jour la caisse, les portes, les paniers... - nous prenons essentiellement les paiements en CB pour éviter de manipuler la monnaie - nous favorisons les commandes par mail -... Bref, on essaie comme on peut d'éviter de propager le virus suite aux décisions du gouvernement ! Merci pour votre compréhension !</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr class="offset-lg-3 col-lg-6 offset-md-3 col-md-6 offset-sm-2 col-sm-8 offset-1 col-10 my-5">
+
+      <div class="row info">
+        <div class="col-lg-10 center">
+          <div class="mb-3">
+            <p class="mb-1">​Pour en savoir plus sur toutes les actualités du magasin, n'hésitez pas à vous abonner à la newsletter !</h4>
+            </br>
+            <p class="mb-1">A très bientôt !</p>
+            <p class="mb-1">Martin, Matthias, Léa, Cyril, Gwenn et Céline</p>
+          </div>
+
+          <div class="mb-3">
+            <h4 class="m-0">​LES EVENEMENTS DE 2020</h4>
+            <p class="mb-1">Tous les mois, nous organisons des ateliers au magasin. Ils sont gratuits mais sur inscription uniquement. Pour vous inscrire, envoyez-nous un mail labonnepioche38@gmail.com et nous vous répondrons rapidement !</p>
+            <p class="mb-1">En revanche, au vu des mesures prises par le gouvernement face à la situation actuelle sanitaire, nous annulons tous les prochains événements à venir jusqu'à nouvel ordre ! Merci pour votre compréhension !</p>
+          </div>
+
+          <div class="mb-3">
+            <img src="../../data/img/La-Bonne-Pioche-16-2.jpg" alt="Intérieur du magasin">
+          </div>
+          
+        </div>
+      </div>
 
       <?php include("../view/footer.php") ?>
 
