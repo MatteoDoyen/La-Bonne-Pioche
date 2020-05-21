f5ee6eef8aac82c64f4a47381c6586d15ee04f36
Matte Doyen <matteo.doyen@hotmail.com>
2020-05-21 20:00:41 +0200

    produitDao insert

--- model/test.php
+++ model/test.php
@@ -8,8 +8,26 @@ $panier = new ProduitDAO();
 // insertProduit($stock, $refProduit, $libelle, $fabricant, $rayon, $famille, $coef, $description,
 //   $origine, $caracteristiques, $prixU, $urlImg, $quantiteU, $unite)
 
+// $panier->insertProduit(5,56,'banane','matteo','fruits','fruitsexotique',1.2,'grosse banane','etat unis','f',5.2,'aucune.jpg',3.2,'gramme');
+// $test = $panier->get(56);
 
-$panier->insertProduit(5,55,'banane','matteo','fruits','fruitsexotique',1.2,'grosse banane','etat unis','f',5.2,'aucune.jpg',3.2,'gramme');
-$test = $panier->get(1);
-print_r($test);
- ?>
+
+?>
+//  CREATE TABLE produits (
+//    stock INTEGER,
+//    refProduit INTEGER PRIMARY KEY,
+//    libelle STRING,
+//    fabricant STRING,
+//    rayon STRING,
+//    famille STRING,
+//    coef FLOAT,
+//    description STRING,
+//    origine STRING,
+//    caracteristiques STRING,
+//    prixU FLOAT,
+//    urlImg STRING,
+//    quantiteU FLOAT,
+//    unite STRING
+//  );
+//
+// insert into produits values(5,55,"banane","matteo","fruits","fruitsexotique",1.2,"grosse banane","etat unis","f",5.2,"aucune.jpg",3.2,"gramme");
