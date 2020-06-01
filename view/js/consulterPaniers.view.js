2e7cb2ce7ab9b1568e1f3791a67b2733eaa76f9e
Matte Doyen <matteo.doyen@hotmail.com>
2020-06-01 13:20:00 +0200

    + graphique consulter panier - alert

--- view/js/consulterPaniers.view.js
+++ view/js/consulterPaniers.view.js
@@ -13,13 +13,19 @@ function envoieFormulaireSupprimer(elt)
 function supprimerPanier(elt)
 {
   id = elt.id.split('_')[1];
-  $('body').prepend('<div id="alertSuppressionBg"></div><div id="alertSuppressionMessage"><p>Étes-vous sûr de vouloir supprimer le panier ?</p><div class=""><button type="button" name="button" id="boutonAlertSupprimer_'+id+'" class="boutonAlertSupprimer" onclick="envoieFormulaireSupprimer(this)" >Oui supprimer</button><button type="button" name="button" id="boutonAlertAnnuler_'+id+'" class="boutonAlertAnnuler" onclick="annulationSuppression()" >Non annuler</button></div></div>');
+  $('body').prepend('<div id="alertSuppressionBg" onclick="annulationSuppression()" ></div><div id="alertSuppressionMessage"><p>Étes-vous sûr de vouloir supprimer le panier ?</p><div class=""><button type="button" name="button" id="boutonAlertSupprimer_'+id+'" class="boutonAlertSupprimer" onclick="envoieFormulaireSupprimer(this)" >Oui supprimer</button><button type="button" name="button" id="boutonAlertAnnuler_'+id+'" class="boutonAlertAnnuler" onclick="annulationSuppression()" >Non annuler</button></div></div>').hide()
+        .fadeIn(400);
   $('body').css('overflow','hidden');
 }
 
 function annulationSuppression()
 {
-  $('#alertSuppressionBg').remove();
-  $('#alertSuppressionMessage').remove();
-  $('body').css('overflow','visible');
+  $('#alertSuppressionBg').fadeOut(400);
+  $('#alertSuppressionMessage').fadeOut(400);
+  setTimeout(function(){
+    $('#alertSuppressionBg').remove();
+    $('#alertSuppressionMessage').remove();
+    $('body').css('overflow','visible');
+}, 400);
+
 }
