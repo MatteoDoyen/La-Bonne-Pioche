2d1f6670c5384501d3945ef085ed372a9436f9d0
BaptSeg <segeat.b@gmail.com>
2020-06-01 18:05:49 +0200

    .

--- controlers/modeDemploi.ctrl.php
+++ controlers/modeDemploi.ctrl.php
@@ -2,7 +2,7 @@
 require_once("../framework/view.class.php");
 
 // On créer une variable view que l'on rattache au fichier accueil.view.php
-$view = new View('../view/connexion.view.php');
+$view = new View('../view/modeDemploi.view.php');
 // On l'affiche (cette méthode est défini dans le fichier ../framework/view.class.php).
 $view->show();
 
